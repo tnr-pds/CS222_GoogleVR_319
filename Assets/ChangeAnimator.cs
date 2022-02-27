fc980787a2b3e4ddddbc66688f74e34b4bba4cc5
tnr-pds <tnr.pds@gmail.com>
2022-02-27 14:26:32 +0700

    first commit

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    
+    public void getAnimatorCroun(){
+        this.GetComponent<Animator>().Play("Cr");
+    }
+
+    public void getAnimatorTaunt(){
+        this.GetComponent<Animator>().Play("Ta");
+    }
+
+}
---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void getRed(){
+         this.GetComponent<Renderer>().material.color = Color.red; 
+    }
+    public void getGreen(){
+        this.GetComponent<Renderer>().material.color = Color.green; 
+    }
+    public void getBlue(){
+        this.GetComponent<Renderer>().material.color = Color.blue; 
+    }
+    
+}
